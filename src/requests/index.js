e0828d1825a0659a3c92b472800cb8f14eb776aa
SushantTandukar <sushant.tandukar@logicabeans.com>
2024-01-16 13:31:04 +0545

    added api endpoint

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://mgmt-second.onrender.com",
 });
 
 export default axiosInstance;
